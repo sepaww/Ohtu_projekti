4b46614d9e54b47905cca3322bf8246a939e7aac
ogkristi <139138898+ogkristi@users.noreply.github.com>
2023-11-22 15:16:44 +0200

    paljasta tietovarastoluokan rajapinta

---
+++ backend/bibrepository.py
@@ -0,0 +1,11 @@
+from model import Entry
+
+class BibRepository:
+    def __init__(self) -> None:
+        pass
+
+    def save(self, new: Entry):
+        pass
+
+    def all(self) -> list[Entry]:
+        pass
