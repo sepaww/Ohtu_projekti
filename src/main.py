a2cefd5f4435a35f556f94abe2c03596230b3f1f
Anni Simonen <anni.simonen@helsinki.fi>
2023-11-22 12:07:08 +0200

    remove

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from UI import run
-
-def main(datab):
-    runner=run.Run(datab)
-    runner.start()
-    print('main running')
-
-
-main('repository.datab.db')
--- src/tests/exists_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class Test_test_existance(unittest.TestCase):
-    def setUp(self):
-        self.val=1
-
-    def test_applying(self):
-        self.assertEqual(self.val, 1)
-
