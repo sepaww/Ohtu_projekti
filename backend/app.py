68ca15160e4b25adc407e18a420cb37e2b8b6752
“l.sommarberg” <laura.sommarberg@gmail.comgit config --global user.email laura.sommarberg@gmail.com>
2023-11-22 15:42:54 +0200

    reitit kommunikoi bib-luokan kanssa

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import jsonify, request, Flask
-# import bibrepository
+from bibrepository import BibRepository
 
 app = Flask(__name__)
 
@@ -10,9 +10,16 @@ ref = [
 
 @app.route('/api/refs')
 def get_refs():
-    return jsonify(ref)
+    bib_repo = BibRepository()
+    refs = bib_repo.all()
+    if refs:
+        return jsonify(refs)
+    else:
+        return jsonify({'error': 'No references found'}), 404
+
 
 @app.route('/api/refs', methods=['POST'])
 def add_refs():
-    ref.append(request.get_json())
+    bib_repo = BibRepository()
+    bib_repo.save(request.get_json())
     return '', 204
