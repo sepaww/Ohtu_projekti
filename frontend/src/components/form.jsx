0dd446257bf51e76a78fb6b86ebdd135c3b9d117
Visa Blad <visa.blad@gmail.com>
2023-11-26 14:32:49 +0200

    Toteutettu lomakkeessen muuttivien kenttien toiminnallisuus viitteiden perusteella

--- frontend/src/components/form.jsx
+++ frontend/src/components/form.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import { Form, InputGroup, Stack, Button} from "react-bootstrap";
 import refservice from "../Services/Refservice";
+import { useState } from "react";
 
 const Inputfield = ({ input }) => {
   return(
@@ -18,17 +19,28 @@ const Inputfield = ({ input }) => {
     </div>
   )
 }
-const Typeselect = () => {
-  const types = ["Book", "Journal", "Article"]
+const Typeselect = ({setRefType}) => {
+  const types = ["Book", "Article", "Booklet", "MasterThesis"]
+  // this prevents a bug if you choose select type to start again 
+  const setTypeCorrect = (target) => {
+    if (target === "reset") {
+      setRefType(null)
+    }
+    else {
+      setRefType(target)
+    }
+  }
   return(
   <div> 
-    <Form.Select aria-label="Default select example" name="type">
+    <Form.Select aria-label="Ref type select" name="type" size="lg" className="m-2" onChange={(e) => setTypeCorrect(e.target.value)}>
+      <option value={"reset"}> Select Type to start</option>
       {types.map((t) => <option value={t} key={t}>{t}</option>)}
     </Form.Select>
   </div>)
 }
 
 const RefForm = ({setRefs, refs}) => {
+    const [reftype, setRefType] = useState(false)
     const handleSubmit =  async(event)   => {
       event.preventDefault()
       const form = event.target
@@ -38,20 +50,35 @@ const RefForm = ({setRefs, refs}) => {
       if (response.status === 201) {
         setRefs(refs.concat(formJson))
       }
-  
-
     }
-    const inputs = [
-          {name: "title", placeholder:"Example Title"}, 
-          {name: "author", placeholder:"Example Author"}, 
-          {name: "journal", placeholder:"Journal Name"}
-        ]
-    
+    const fields = [
+      {name: "title", placeholder:"Example Title"}, 
+      {name: "author", placeholder:"Example Author"}, 
+      {name: "journal", placeholder:"Journal Name"},
+      {name: "year", placeholder: "Year published"},
+      {name: "volume", placeholder: ""},
+      {name: "nuber", placeholder: "6"},
+      {name: "pages", placeholder: "111-222"},
+      {name: "publisher", placeholder: "WSOY AB"},
+      {name: "adress", placeholder: "Mannerheimintie 1"},
+      {name: "howpubluihsed", placeholder: "Distributed in unicafe"}, 
+      {name: "Month", placeholder: "Jan"},
+      {name: "School", placeholder: "Uni Of Helsingi"}
+    ]
+    const inputs = {
+      Book: [fields[1], fields[0],fields[7],fields[8], fields[3]], 
+      Article: [fields[0], fields[1],fields[5],fields[6]],
+      Booklet: [fields[0], fields[1],fields[9], fields[10], fields[6]],
+      MasterThesis: [fields[0], fields[1],fields[11], fields[6], fields[7], fields[10]]
+      }
+    console.log(reftype)
+
     return (
+      
         <Form method="post" onSubmit={handleSubmit}> 
+        <Typeselect setRefType={setRefType}/>
             <Stack className="col-md-5 mx-left" gap="3">
-              {inputs.map((input) => <Inputfield key={input.name} input={input}> </Inputfield>)}
-            <Typeselect/>
+              {reftype ? inputs[reftype].map((input) => <Inputfield key={input.name} input={input}> </Inputfield>) : <h5> Start by selecting ref type</h5>}
             </Stack>
             <Button type="submit">Submit Reference</Button>
 
