c80fe480611d102cd37ef3fa6050acc8df72de7e
ogkristi <139138898+ogkristi@users.noreply.github.com>
2023-11-29 22:36:21 +0200

    & unohtu

--- robot_script.sh
+++ robot_script.sh
@@ -3,7 +3,7 @@
 npm install --prefix frontend
 npm run build --prefix frontend
 
-poetry run gunicorn --chdir backend -b 127.0.0.1:5173 'app:create_app()'
+poetry run gunicorn --chdir backend -b 127.0.0.1:5173 'app:create_app()' &
 
 # odetetaan, että palvelin on valmiina ottamaan vastaan pyyntöjä
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5173/ping)" != "200" ]];
