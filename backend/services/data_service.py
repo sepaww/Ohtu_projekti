fc36de395888c7ae9dae2ea497aa1901c8c5bb86
ogkristi <139138898+ogkristi@users.noreply.github.com>
2023-11-29 09:05:51 +0200

    poista arpominen artikkelin luonnista

--- backend/services/data_service.py
+++ backend/services/data_service.py
@@ -1,20 +1,13 @@
 from models.article import Article
-import random
 
 
-class DataService():
-
+class DataService:
     def get_all(self):
         return Article.all()
-    
+
     # tähän voi lisätä myös eri viitetyyppien tallennuksen
     def save_data(self, payload):
-        new = Article(
-            citekey=str(random.randint(0, 10000)),
-            author=payload["author"],
-            title=payload["title"],
-            year=str(random.randint(0, 1000)),
-            journal=payload["journal"],
-        )
-        new.save()
-        return new
+        if payload.pop("type") == "article":
+            new = Article(**payload)
+            new.save()
+            return new
