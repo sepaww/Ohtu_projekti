35473a2c9b499d1de7cc2f51ae4187b5087d82e7
lsommarberg <laura.sommarberg@gmail.com>
2023-11-29 12:06:02 +0200

    yksikkötestit routes ja data_service

--- backend/tests/test_bib_controller.py
+++ backend/tests/test_bib_controller.py
@@ -1,23 +1,91 @@
 import unittest
-
-
-from flask import Flask
-from controllers.bib_controller import bib_controller
+from flask import Flask, json
+from app import bib_controller, db
+from models.article import Article
+from services.data_service import DataService
+from unittest.mock import patch, Mock
 
 
 class BibControllerTestCase(unittest.TestCase):
-
     def setUp(self):
         self.app = Flask(__name__)
-        self.app.config['TESTING'] = True
+        self.app.config["TESTING"] = True
+        self.app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+        self.app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        with self.app.app_context():
+            db.init_app(self.app)
+            db.create_all()
         self.app.register_blueprint(bib_controller)
 
         self.client = self.app.test_client()
 
-    def test_get(self):
-        response = self.client.get('/api/refs')
+    def tearDown(self):
+        with self.app.app_context():
+            db.session.remove()
+            db.drop_all()
+
+    @patch("services.data_service.DataService.get_all")
+    def test_get(self, mock_get_all):
+        mock_get_all.return_value = {
+            "Title": "example",
+            "Author": "Some_author",
+            "Journal": "HS ",
+            "type": "Journal",
+        }
+        response = self.client.get("/api/refs")
         self.assertEqual(response.status_code, 200)
 
+        data = json.loads(response.data)
+        self.assertIn("form", data)
+        form_data = data["form"]
+        expected_form_data = {
+            "article": ["citekey", "author", "title", "year", "journal"]
+        }
 
-if __name__ == '__main__':
+        self.assertEqual(form_data, expected_form_data)
+
+    @patch("services.data_service.DataService.save_data")
+    def test_save_data(self, mock_save_data):
+        mock_save_data.return_value = {
+            "Title": "example",
+            "Author": "Some_author",
+            "Journal": "HS ",
+            "type": "Journal",
+        }
+        data_to_be_saved = {
+            "Title": "example",
+            "Author": "Some_author",
+            "Journal": "HS ",
+            "type": "Journal",
+        }
+        response = self.client.post("/api/refs", json=data_to_be_saved)
+        self.assertEqual(response.status_code, 201)
+        data = response.get_json()
+        self.assertEqual(
+            data,
+            {
+                "Title": "example",
+                "Author": "Some_author",
+                "Journal": "HS ",
+                "type": "Journal",
+            },
+        )
+
+    @patch("services.data_service.DataService.delete_article")
+    def test_delete_ref_success(self, mock_delete_article):
+        mock_delete_article.return_value = True
+        response = self.client.delete('/api/refs/example_citekey' , content_type='application/json')
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(response.get_data(as_text=True), '')   
+
+    @patch("services.data_service.DataService.delete_article")
+    def test_delete_ref_failure(self, mock_delete_article):
+        mock_delete_article.return_value = False
+        response = self.client.delete('/api/refs/example_citekey' , content_type='application/json')
+        self.assertEqual(response.status_code, 500)
+        expected_response = {"message": f"Failed to delete article with citekey."}
+        self.assertEqual(json.loads(response.get_data(as_text=True)), expected_response)
+
+
+if __name__ == "__main__":
     unittest.main()
---
+++ backend/tests/test_data_service.py
@@ -0,0 +1,54 @@
+import unittest
+from unittest.mock import patch, Mock
+from services.data_service import DataService, Article
+from controllers.bib_controller import delete_ref
+
+
+class TestDataService(unittest.TestCase):
+    @patch("services.data_service.Article")
+    def test_get_all(self, mock_article):
+        mock_article.all.return_value = ["article1", "article2", "article3"]
+
+        data_service = DataService()
+        result = data_service.get_all()
+
+        self.assertEqual(result, ["article1", "article2", "article3"])
+        mock_article.all.assert_called_once()
+
+    @patch("services.data_service.Article")
+    def test_save(self, mock_article):
+        data_service = DataService()
+        payload = {"title": "example", "author": "Some author", "journal": "HS "}
+        mock_article.return_value = Mock(
+            citekey="123",
+            author="Some author",
+            title="example",
+            year="1999",
+            journal="HS",
+        )
+        result = data_service.save_data(payload)
+        mock_article.assert_called_once()
+
+        self.assertEqual(result.author, "Some author")
+        self.assertEqual(result.title, "example")
+
+    @patch("services.data_service.Article.delete_by_citekey")
+    def test_delete_article_success(self, mock_delete_by_citekey):
+        data_service = DataService()
+        result = data_service.delete_article(citekey="example_citekey")
+        mock_delete_by_citekey.assert_called_once_with("example_citekey")
+        self.assertTrue(result)
+
+    @patch(
+        "services.data_service.Article.delete_by_citekey",
+        side_effect=Exception("Mocked error"),
+    )
+    def test_delete_article_failure(self, mock_delete_by_citekey):
+        data_service = DataService()
+        result = data_service.delete_article(citekey="example_citekey")
+        mock_delete_by_citekey.assert_called_once_with("example_citekey")
+        self.assertFalse(result)
+
+
+if __name__ == "__main__":
+    unittest.main()
