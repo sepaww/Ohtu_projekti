27c7331f3d20f6b06b96b6b12cc6d327cdac0564
ogkristi <139138898+ogkristi@users.noreply.github.com>
2023-12-06 00:34:17 +0200

    efekti korjaus

--- frontend/src/components/timedalert.jsx
+++ frontend/src/components/timedalert.jsx
@@ -5,12 +5,13 @@ import { Alert } from 'react-bootstrap'
 export default function TimedAlert({ msg }) {
     const [show, setShow] = useState(false)
 
+    // eslint-disable-next-line
     useEffect(() => {
         if (msg.variant !== "boot") {
             setShow(true)
             setTimeout(() => setShow(false), 3000)
         }
-    }, [msg.variant])
+    })
 
     return (
         <Alert show={show} variant={msg.variant}>{msg.text}</Alert>
